252ed538b67d2008ac2a44046b81d7b2a9ebb78f
EC2 Default User <ec2-user@ip-172-31-12-204.us-east-2.compute.internal>
2021-07-03 03:16:06 +0000

    committed

---
+++ 0987/src/main/java/3/application/module/response/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Response POJOs
+ */
+package 3.application.module.response;
---
+++ 0987/src/main/java/3/application/module/service/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Interfaces of the exposed services of module.
+ */
+package 3.application.module.service;
